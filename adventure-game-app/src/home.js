d685137d46ae80247f168579e73b27cf5feebefc
Jesse Ma <j.masonoma17@gmail.com>
2018-06-28 15:28:31 -0700

    Initial commit

---
+++ adventure-game-app/src/home.js
@@ -0,0 +1,12 @@
+import './home.css';
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <h1> Test test </h1>
+        )
+    }
+}
+
+export default Home;
