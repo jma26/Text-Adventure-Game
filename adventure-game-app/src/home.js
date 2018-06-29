17154a0b4bacad01c50cd8196fbb1a041a07ab67
Jesse Ma <j.masonoma17@gmail.com>
2018-06-29 15:26:41 -0700

    Update Home component's state with showDeath as a boolean, and display Death component if true/clicked

--- adventure-game-app/src/home.js
+++ adventure-game-app/src/home.js
@@ -1,19 +1,40 @@
 import './home.css';
+import Death from './death.js';
 import React from 'react';
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: this.props.name,
+            profession: this.props.profession,
+            showDeath: false,
+        }
+        this.showDeathComponent = this.showDeathComponent.bind(this);
+    }
+
+    showDeathComponent() {
+        this.setState({
+            showDeath: true,
+        });
+    }
+
     render() {
+        if (this.state.showDeath && this.state.name !== '' && this.state.profession !== null) {
+            return <Death name={this.state.name} profession={this.state.profession} />
+        }
+
         return (
             <div className="content">
                 <div className="info-container">
                     <div className="player-info">
-                        <h3> Player name: {this.props.name} </h3>
-                        <h3> Profession: {this.props.profession} </h3>
+                        <h3> Player name: {this.state.name} </h3>
+                        <h3> Profession: {this.state.profession} </h3>
                     </div>
                     <p className="story"> You wake up in a dark room and your memory is a bit hazy. Light is coming through your window and you smell something is cooking or burning </p>
                 </div>
                 <div className="player-options">
-                    <button> Go back to sleep </button>
+                    <button onClick={this.showDeathComponent}> Go back to sleep </button>
                     <button> Look outside the window </button>
                     <button> Don't care! Grab your gear and walk towards the door without checking </button>
                 </div>
