ce6d744795c27efc87ab9a35852c6c8b69556ae1
Jesse Ma <j.masonoma17@gmail.com>
2018-06-29 14:43:34 -0700

    Update Home class with content and buttons for player-options

--- adventure-game-app/src/home.js
+++ adventure-game-app/src/home.js
@@ -4,7 +4,20 @@ import React from 'react';
 class Home extends React.Component {
     render() {
         return (
-            <h1> Test test </h1>
+            <div className="content">
+                <div className="info-container">
+                    <div className="player-info">
+                        <h3> Player name: {this.props.name} </h3>
+                        <h3> Profession: {this.props.profession} </h3>
+                    </div>
+                    <p className="story"> You wake up in a dark room and your memory is a bit hazy. Light is coming through your window and you smell something is cooking or burning </p>
+                </div>
+                <div className="player-options">
+                    <button> Go back to sleep </button>
+                    <button> Look outside the window </button>
+                    <button> Don't care! Grab your gear and walk towards the door without checking </button>
+                </div>
+            </div>
         )
     }
 }
