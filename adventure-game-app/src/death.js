e85df1129e1351821ef2c4c8bb6a44abbd98e771
Jesse Ma <j.masonoma17@gmail.com>
2018-07-01 00:46:40 -0700

    Update Death component to render fire content if reason is fire

--- adventure-game-app/src/death.js
+++ adventure-game-app/src/death.js
@@ -10,10 +10,35 @@ class Death extends React.Component {
         };
     }
     render() {
+        if (this.props.reason === 'fire') {
+            return (
+                <div className="content">
+                    <div className="info-container">
+                        <div className="player-info">
+                            <h3> Player name: {this.state.name} </h3>
+                            <h3> Profession: {this.state.profession} </h3>
+                        </div>
+                        <div className="death-container">
+                            <h1> Game over! </h1>
+                            <p> You died from {this.props.reason}. Little did you know your building was on fire and you were slowly engulfed in flames....Better luck next time </p>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+
         return (
-            <div>
-            <h1> Test </h1>
-            <h1> {this.state.name} as a {this.state.profession} </h1>
+            <div className="content">
+                <div className="info-container">
+                    <div className="player-info">
+                        <h3> Player name: {this.state.name} </h3>
+                        <h3> Profession: {this.state.profession} </h3>
+                    </div>
+                    <div className="story">
+                        <h1> Game over! </h1>
+                        <h1> You died from {this.props.reason} </h1>
+                    </div>
+                </div>
             </div>
         )
     }
